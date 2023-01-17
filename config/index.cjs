1a9413ef2a8589ff4f4a37e8bc83ee4fa48556bc
deagman <49191424+deagman@users.noreply.github.com>
2023-01-17 13:13:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oHVV_6R1rm-Eh9mdo2NuEPyLU3xU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ua6zw1dUoddA7C8GYiaoxTJ1sPU7LKF8VW4mX0LhNXI',
+      useTemplateId: '0cnZjkflTh2Shed23hXtOZn_KpHZDcmO_ZJtwfL0m_s',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
@@ -32,7 +32,7 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-02',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
+      horoscopeDateType: '本周',
       festivals: [
         /* 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -41,10 +41,10 @@ const USER_CONFIG = {
         */
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: 'Wendy的法定', year: '2006', date: '07-18',
+          type: '生日', name: 'Wendy法定', year: '2006', date: '07-18',
         },
         {
-          type: '生日', name: 'Wendy的自然', year: '2006', date: '09-02',
+          type: '生日', name: 'Wendy自然', year: '2006', date: '09-02',
         },
         {
           type: '节日', name: '647号宇宙的诞生', year: '2023', date: '01-01',
@@ -58,22 +58,6 @@ const USER_CONFIG = {
         // { keyword: 'marry_day', date: '2022-09-09' },
         // 来到647号宇宙的日子
            { keyword: 'coming_day', date: '2023-01-01'}
-      ],
-      slotList: [
-        // 这样配置的话，就会每次发送这句话
-        { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
-        // 这样配置的话，就会每次随机选一句话发送
-        {
-          keyword: 'lover_prattle',
-          contents: [
-            '因为太喜欢你，所以看谁都像是情敌。',
-            '申请成为你爱里的永久居民。',
-            '你很傻，你很笨，可我还是很羡慕你，因为你有我',
-            '遇见你，就好像捡到了100斤的运气'
-          ],
-        }
-        // 你可以不断按格式往下增加
-        // ...
       ],
     },
     {
@@ -91,7 +75,23 @@ const USER_CONFIG = {
       id: '',
     }
   ],
-
+  
+  SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
 }
 
 module.exports = USER_CONFIG

854df123b8be3395b423f5080e6058465aadf212
deagman <49191424+deagman@users.noreply.github.com>
2023-01-17 14:53:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oHVV_6R1rm-Eh9mdo2NuEPyLU3xU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'I-X1YhPkluetUV_h5COIrKI0W5nNUK3XcD30kns-sLw',
+      useTemplateId: 't2LV6lGAJ4oqy-h7TZ_fmdwTDyvrxxnsEE4NXIfXQU8',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
